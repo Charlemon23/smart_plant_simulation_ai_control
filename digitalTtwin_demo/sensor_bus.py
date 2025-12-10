a4c613e4cd33301011b15c2591ffee6b8c703c47
Charlemon23 <158347598+Charlemon23@users.noreply.github.com>
2025-12-10 09:28:50 -0600

    Add SensorBus class for reading noisy sensor values

--- digitalTtwin_demo/sensor_bus.py
+++ digitalTtwin_demo/sensor_bus.py
@@ -1,1 +1,12 @@
+import numpy as np
+
+class SensorBus:
+    def __init__(self, noise_level=0.01):
+        self.noise = noise_level
+
+    def read(self, real_values):
+        noisy = {}
+        for k, v in real_values.items():
+            noisy[k] = v + np.random.normal(0, self.noise * v)
+        return noisy
 
