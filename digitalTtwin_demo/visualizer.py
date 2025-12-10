aa22b7200e121b689163d62ab50fdee998566fdd
Charlemon23 <158347598+Charlemon23@users.noreply.github.com>
2025-12-10 09:30:14 -0600

    Add Visualizer class for plotting data

--- digitalTtwin_demo/visualizer.py
+++ digitalTtwin_demo/visualizer.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+
+class Visualizer:
+    def __init__(self):
+        self.T = []
+        self.C = []
+
+    def update(self, T, C):
+        self.T.append(T)
+        self.C.append(C)
+
+    def show(self):
+        plt.figure(figsize=(10,5))
+        plt.plot(self.T, label="Temperature (K)")
+        plt.plot(self.C, label="Concentration (mol/L)")
+        plt.legend()
+        plt.title("Digital Twin State Over Time")
+        plt.xlabel("Timestep")
+        plt.grid()
+        plt.show()
 
